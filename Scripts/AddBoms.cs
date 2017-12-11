bdb4e6641a0789d9633d439256da6efd3a8ab091
poikelp <takukelp@gmail.com>
2017-12-12 00:02:14 +0900

    first

---
+++ Scripts/AddBoms.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBoms : MonoBehaviour {
+
+
+	private GameObject bom;
+
+	// Use this for initialization
+	void Start () {
+		bom = (GameObject)Resources.Load ("Prefab/bom");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			transform.position = new Vector3 (getRandomfloat() , 0, getRandomfloat() );
+			Instantiate (bom, transform.position, transform.rotation);
+		}
+	}
+
+	float getRandomfloat () {
+		return Random.Range (-9, 9);
+	}
+}
---
+++ Scripts/SphereMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMaker : MonoBehaviour {
+	private GameObject sphere;
+	[SerializeField]
+	private int spheresMass = 50;
+	private int sphereCount = 0;
+	[SerializeField]
+	private float coolTime = 1.0f;
+	private float time; 
+
+	// Use this for initialization
+	void Start () {
+		sphere = (GameObject)Resources.Load ("Prefab/sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sphereCount < spheresMass) {
+			if (time > coolTime) {
+				Instantiate (sphere, transform.position, transform.rotation);
+				transform.position = new Vector3 (getRandomfloat() , 0, getRandomfloat() );
+				time = 0;
+				sphereCount++;
+
+			}
+		}
+		time += Time.deltaTime;
+	}
+
+	float getRandomfloat () {
+		return Random.Range (-9, 9);
+	}
+}
