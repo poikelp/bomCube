c574b488637d434305661a4dac443ff4338d8be5
poikelp <takukelp@gmail.com>
2017-12-12 01:29:20 +0900

    clones kill

---
+++ Scripts/DestroyMe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (transform.root.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
